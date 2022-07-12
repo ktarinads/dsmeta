213d0ca7689529efb4ed7cfe02594f6a4f39f0cf
csDarpa <60275825+ktarinads@users.noreply.github.com>
2022-07-12 22:38:35 +0000

    More components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <NotificationButton />
-      
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            {/* aqui vai vir o componente do card maior da página após o header */}
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
+
     </>
   )
 }
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,9 @@
-import icon from '../../assets/img/notification-icon.svg'
-
 import './styles.css'
 
 function NotificationButton() {
     return (
         <div className="dsmeta-red-btn">
-            <img src="{icon}" alt="Notificar" />
+            <img src="/src/assets/img/notification-icon.svg" alt="Notificar" />
         </div>
     )
 }
