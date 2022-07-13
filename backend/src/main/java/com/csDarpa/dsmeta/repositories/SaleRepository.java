0209d31b148447faf22d380e1a802d486b3dbe8a
csDarpa <60275825+ktarinads@users.noreply.github.com>
2022-07-13 18:03:52 +0000

    Date select

--- backend/src/main/java/com/csDarpa/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/csDarpa/dsmeta/repositories/SaleRepository.java
@@ -1,10 +1,17 @@
 package com.csDarpa.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.csDarpa.dsmeta.entities.Sale;
 
 //esse vai ser o componente responsavel por acessar o db
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
+	//comando JPQL - adaptacao de SQL pro JPA
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/csDarpa/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/csDarpa/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.csDarpa.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.csDarpa.dsmeta.entities.Sale;
@@ -14,7 +18,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll(); //vai buscar todas as vendas
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		//tratando o default, criando data com dia de hoje
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//converter as datas de texto pra local date
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable); //vai buscar todas as vendas
 	}
 }
