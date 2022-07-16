c2343a7acbe038825cc3ad543476b403450035e2
csDarpa <60275825+ktarinads@users.noreply.github.com>
2022-07-16 18:44:01 +0000

    Notification button sms feature done

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,22 @@
+import icon from '/src/assets/img/notification-icon.svg';
 import './styles.css';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("DONE");
+        });
+}
+
+function NotificationButton({saleId} : Props ) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src="/src/assets/img/notification-icon.svg" alt="Notificar" />
         </div>
     )
