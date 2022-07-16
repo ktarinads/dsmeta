e8f4a9cf84a5c0cb1384a836deac5a382ef663f9
csDarpa <60275825+ktarinads@users.noreply.github.com>
2022-07-16 20:13:19 +0000

    Svg bug correction done

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,25 +1,28 @@
-import './styles.css';
 import axios from 'axios';
-import { BASE_URL } from '../../utils/request';
 import { toast } from 'react-toastify';
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+
+import'./styles.css';
 
 type Props = {
     saleId: number;
 }
 
-function handleClick(id :number){
+function handleClick(id : number){
     axios(`${BASE_URL}/sales/${id}/notification`)
-        .then(response => {
-            toast.info("SMS enviado com sucesso");
-        });
+    .then(response => {
+        toast.info("SMS enviado com sucesso");
+    });
 }
 
-function NotificationButton({saleId} : Props ) {
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
-            <img src="../../../src/assets/img/notification-icon.svg" alt="Notificar" />
+        <div className="dsmeta-red-btn" onClick={()=> {handleClick(saleId)}}>
+            <img src={icon} alt="Notificar" />
         </div>
     )
+
 }
 
 export default NotificationButton;
