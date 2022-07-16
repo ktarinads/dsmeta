169715900447b083ff1b7ad7682dca941f7cb833
csDarpa <60275825+ktarinads@users.noreply.github.com>
2022-07-16 18:49:48 +0000

    Toast feat done

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -4,5 +4,4 @@ export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:80
 // o import.meta.env. é uma variável de ambiente, que carrega a informação de 
 // E o ?? é um operador que significa que o valor do lado direito tem um valor padrão da variável, porém ela vai pegar 
 // o valor que está à esquerda. se não tiver variavel de ambiente com esse nome, por padrão, utilize o localhost:8080
-// esse macete é pra fazer o frontend pegar o valor local caso nao tenha instalado o front num lugar q ainda n tenha env variables
- 
+// esse mace    te é pra fazer o frontend pegar o valor local caso nao tenha instalado o front num lugar q ainda n tenha env variables
